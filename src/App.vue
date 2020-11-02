bf5fa716fddbc93ce3d04ee9ae9afa1379f47eb7
ElevateBart <ledouxb@gmail.com>
2020-11-02 11:36:41 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <VueLive code="<input />" />
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import "vue-live/lib/vue-live.esm.css";
+import { VueLive } from "vue-live";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    VueLive,
+  },
+};
+</script>
---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { SharedConfig } from "vite";
+
+const config: SharedConfig = {
+  alias: {
+    vue: "vue/dist/vue.esm-browser.js",
+  },
+};
+
+export default config;
